af578b70d42ba9ce35ecab26bac7a1ff241afe2a
Henry <sergenovak92@gmail.com>
2023-02-28 17:44:58 +0300

    added routing with react-router-dom

---
+++ src/components/pages/MainPage.js
@@ -0,0 +1,34 @@
+import { useState } from "react";
+
+import ErrorBoundary from "../errorBoundary/ErrorBoundary";
+import RandomChar from "../randomChar/RandomChar";
+import CharList from "../charList/CharList";
+import CharInfo from "../charInfo/CharInfo";
+
+import decoration from '../../resources/img/vision.png';
+
+export const MainPage = () => {
+
+    const [selectedChar, setChar] = useState(null);
+
+    const onSelectedChar = (id) => {
+        setChar(id)
+    }
+
+    return(
+        <>
+            <ErrorBoundary>
+                <RandomChar/>
+            </ErrorBoundary>
+            <div className="char__content">
+                <ErrorBoundary>
+                    <CharList onSelectedChar={onSelectedChar}/>
+                </ErrorBoundary>
+                <ErrorBoundary>
+                    <CharInfo chardId={selectedChar}/>
+                </ErrorBoundary>
+            </div>
+            <img className="bg-decoration" src={decoration} alt="vision"/>
+        </>
+    )
+}
